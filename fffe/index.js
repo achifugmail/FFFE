7ce9988b79127c0423723387a0556807b053a9d6
Andrei Chifu <achifu@outlook.com>
2025-03-08 18:56:03 -0800

    3/8

--- fffe/index.js
+++ fffe/index.js
@@ -1,19 +1,19 @@
 const config = {
-    backendUrl: 'https://localhost:44390/api',
-    //backendUrl: 'https://ffbe-akdrgxb2fjd6fddh.eastus2-01.azurewebsites.net/api',
-   
+    //backendUrl: 'https://localhost:44390/api',
+    backendUrl: 'https://ffbe-akdrgxb2fjd6fddh.eastus2-01.azurewebsites.net/api',
 };
 
 document.getElementById('loginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
-    
+
     const username = document.getElementById('loginUsername').value;
     const password = document.getElementById('loginPassword').value;
 
     const response = await fetch(`${config.backendUrl}/User/login`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': 'true'
         },
         credentials: 'include', // Include cookies in the request
         body: JSON.stringify({ username, password })
@@ -21,14 +21,10 @@ document.getElementById('loginForm').addEventListener('submit', async function (
 
     if (response.ok) {
         const data = await response.json();
-        //alert('Login successful!');
-        // Store the user's name in local storage
         localStorage.setItem('username', username);
         localStorage.setItem('userId', data.id);
-        // Redirect to main.html upon successful login
         window.location.href = 'main.html';
     } else {
         alert('Login failed. Please check your username and password.');
     }
 });
-
