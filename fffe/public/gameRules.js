c531240133fabfe08bd14c2e62a6ffbe9beb3cf2
Andrei Chifu <achifu@outlook.com>
2025-04-02 21:48:24 -0700

    rules

---
+++ fffe/public/gameRules.js
@@ -0,0 +1,32 @@
+import config from './config.js';
+import { addAuthHeader } from './config.js';
+
+document.addEventListener('DOMContentLoaded', async function () {
+    async function fetchAndDisplayScoringRules() {
+        try {
+            const response = await fetch(`${config.backendUrl}/ScoringRules/getScoringRules`, addAuthHeader());
+
+            if (!response.ok) {
+                console.error('Failed to fetch scoring rules:', response.status, response.statusText);
+                return;
+            }
+
+            const scoringRules = await response.json();
+            const scoringRulesTable = document.getElementById('scoringRulesTable').getElementsByTagName('tbody')[0];
+            scoringRulesTable.innerHTML = ''; // Clear existing table
+
+            scoringRules.forEach(rule => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${rule.longDescription}</td>
+                    <td>${rule.points}</td>
+                `;
+                scoringRulesTable.appendChild(row);
+            });
+        } catch (error) {
+            console.error('Error fetching scoring rules:', error);
+        }
+    }
+
+    await fetchAndDisplayScoringRules();
+});
--- fffe/public/squad.js
+++ fffe/public/squad.js
@@ -1,5 +1,4 @@
-﻿
-import config from './config.js';
+﻿import config from './config.js';
 import { addAuthHeader } from './config.js';
 let draftPeriodStartDate = null;
 let outPlayerId = null;
